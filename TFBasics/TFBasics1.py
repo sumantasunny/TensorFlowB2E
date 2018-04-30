96cd242906428a8b2e1f0416b27973ea7cd18283
sumanta.sunny@gmail.com <sumanta.sunny@gmail.com>
2018-04-30 21:17:22 +0530

    ok2

--- TFBasics/TFBasics1.py
+++ TFBasics/TFBasics1.py
@@ -1,8 +1,15 @@
 import tensorflow as tf
+import matplotlib as plt
 
 
-def main():
-    print "Hello World!"
+x1 = tf.constant([1,2,3,4])
+x2 = tf.constant([4,3,2,1])
 
-if __name__ == '__main__':
-    main()
+x3 = tf.multiply(x1, x2)
+#x4 = [x3]
+tfprt = tf.Print(x3, [x3, tf.shape(x3), "OK !! : "])
+#x4 = tf.multiply(tfprt, x3)
+#print x4
+
+sess = tf.Session()
+sess.run(tfprt)

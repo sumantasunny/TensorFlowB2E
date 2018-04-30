524525b4476669b1ee099f24bb62ca4de52ecc65
sumanta.sunny@gmail.com <sumanta.sunny@gmail.com>
2018-04-30 21:31:49 +0530

    ok3

--- TFBasics/TFBasics1.py
+++ TFBasics/TFBasics1.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-import matplotlib as plt
-
 
 x1 = tf.constant([1,2,3,4])
 x2 = tf.constant([4,3,2,1])
@@ -10,6 +8,13 @@ x3 = tf.multiply(x1, x2)
 tfprt = tf.Print(x3, [x3, tf.shape(x3), "OK !! : "])
 #x4 = tf.multiply(tfprt, x3)
 #print x4
+x4 = [10, 10, 10, 10]
+
+tfadd = tf.add(tfprt, x4)
+tfprt2 = tf.Print(tfadd, [tfadd])
 
 sess = tf.Session()
-sess.run(tfprt)
+sess.run(tfprt2)
+sess.run(x3)
+print sess.run(tf.add(x1, x2))
+sess.close()

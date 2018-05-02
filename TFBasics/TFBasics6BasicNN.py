880b67d279420ce2934369414e0cfdf7e1e837d4
sumanta.sunny@gmail.com <sumanta.sunny@gmail.com>
2018-05-02 17:30:35 +0000

    OK8

--- TFBasics/TFBasics6BasicNN.py
+++ TFBasics/TFBasics6BasicNN.py
@@ -1,11 +1,32 @@
 import tensorflow as tf
 
 class MyNN():
-	def __init__(self):
-		pass
+	def __init__(self, p_shape, p_scope="Train"):
+		with tf.variable_scope(p_scope, reuse=tf.AUTO_REUSE):
+			self.w = tf.get_variable(name="weights_1", initializer=tf.random_normal(shape=p_shape))
+			self.b = tf.ones(name="bias_1", shape=p_shape[0])
+			self.cost = None
 
-	def test(self):
-		pass
+	def train(self, p_x, p_y):
+			self.x = p_x
+			self.y = p_y
+			mwx = tf.matmul(self.w, self.x)
+			mwxab = tf.add(mwx, self.b)
+			v_sum = tf.reduce_sum(mwxab)
+			self.out = tf.sigmoid(v_sum)
+			self.cost = tf.pow(tf.subtract(self.y, self.out), 2.0)
+			return self.cost
 
-nn = MyNN()
-nn
+nn = MyNN([2,1])
+#optm = tf.train.AdamOptimizer().minimize(nn.cost)
+
+tfinit = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	x1 = [[2.,3.]]
+	y1 = [1.]
+	sess.run(tfinit)
+	cost = nn.train(x1, y1)
+	print sess.run(cost)
+	#print cost
+	#print(sess.run(optm))
---
+++ TFBasics/TFBasics8BasicNN3.py
@@ -0,0 +1,40 @@
+import tensorflow as tf
+
+tf.set_random_seed(100)
+
+num_of_input_units = 2
+num_of_output_units = 1
+
+x = [[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]]
+y = [[1], [-1]]
+num_of_input_units = len(x[0])
+
+v_input_x = tf.placeholder(dtype=tf.float32, shape=[None, num_of_input_units])
+v_input_y = tf.placeholder(dtype=tf.float32, shape=[None, num_of_output_units])
+
+v_weights = tf.get_variable(name="Weights_1", shape=[num_of_input_units, num_of_output_units], initializer=tf.random_normal_initializer())
+v_bias = tf.ones(shape=[num_of_output_units])
+
+v_score = tf.add(tf.matmul(v_input_x, v_weights), v_bias)
+
+v_output = tf.sigmoid(v_score)
+
+v_cost = tf.sqrt(tf.reduce_mean(tf.pow(tf.subtract(v_score, v_input_y), 2)))
+
+v_optimzer = tf.train.AdamOptimizer(learning_rate=0.05).minimize(v_cost)
+
+v_init = tf.global_variables_initializer()
+
+with tf.Session() as sess:
+	sess.run(v_init)
+	print(sess.run(v_weights))
+	print(sess.run(v_bias))
+	#output, cost = sess.run([v_output, v_cost], feed_dict={v_input_x:x, v_input_y: y})
+	cost = 0
+	for i in range(100):
+		for j in range(0, len(x)):
+			xj = [x[j]]
+			yj = [y[j]]
+			_, c = sess.run([v_optimzer, v_cost], feed_dict={v_input_x: xj, v_input_y: yj})
+			cost += c
+		print(cost/len(x))
